800f4805022dd516398655c325064517ac1a80f2
Paul Wild <paulwild@gmail.com>
2021-01-30 18:10:58 +0000

    fouc

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Layout from "/src/Components/layout"
 import "prismjs/themes/prism-okaidia.css"
+import {GlobalStyle} from "/src/Components/layout";
 
 export const wrapPageElement = ({ element, props }) => {
     return(
