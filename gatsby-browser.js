3ccd0305a0dce71dd87e85b8f2aba8420f3fd778
Paul Wild <paulwild@gmail.com>
2021-01-30 17:19:25 +0000

    hmm

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Layout from "./src/components/layout"
+import Layout from "/src/Components/layout"
 import "prismjs/themes/prism-okaidia.css"
 
 export const wrapPageElement = ({ element, props }) => {
