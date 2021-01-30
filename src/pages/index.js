388fac403d3b0f521cac38ce50f799cf336ff926
Paul Wild <paulwild@gmail.com>
2021-01-30 17:36:24 +0000

    Remove redirct, flashes up the 404 page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,49 @@
-import React from "react"
+import * as React from "react"
 import { graphql } from "gatsby"
-import styled from "styled-components"
+import Tags from "../Components/tags"
+import StyledLink from "../Components/styledLink";
+import Header from "../Components/header";
 
 
 
-const About = ({ data }) => {
-    return (<>
-        nope
-    </>)
+const Post = ({slug, publishDate, title, childContentfulBlogPostBodyTextNode, tags}) => {
+    return (
+        <section>
+            <Header>
+                <h1>{title}</h1>
+                <h5>{publishDate}</h5>
+            </Header>
+            <p>{childContentfulBlogPostBodyTextNode.childMarkdownRemark.excerpt} <StyledLink to={`/posts/${slug}`}>Read more</StyledLink></p>
+            <Tags tags={tags}></Tags>
+        </section>
+    )
 }
 
-export default About
+const Posts = ({data}) => {
+    return (<>
+        {data.allContentfulBlogPost.edges.map(post => Post(post.node))}
+    </>)
+
+}
+
+export default Posts;
+
+export const pageQuery = graphql`
+    query {
+        allContentfulBlogPost {
+            edges {
+                node {
+                    slug
+                    publishDate(formatString: "DD MMM YYYY")
+                    title
+                    tags
+                    childContentfulBlogPostBodyTextNode {
+                        childMarkdownRemark {
+                            excerpt(pruneLength: 400)
+                        }
+                    }
+                }
+            }
+        }
+    }
+`
